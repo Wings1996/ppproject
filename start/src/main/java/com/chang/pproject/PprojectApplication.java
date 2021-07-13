f3bd58d59c3f79ea4972067380405597f43e2879
Chang <V_AS8QY2@dir.saicgmac.com>
2021-07-13 15:05:55 +0800

    pproject first commit

---
+++ start/src/main/java/com/chang/pproject/PprojectApplication.java
@@ -0,0 +1,13 @@
+package com.chang.pproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PprojectApplication.class, args);
+    }
+
+}
